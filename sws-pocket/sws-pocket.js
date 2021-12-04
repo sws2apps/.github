f2eb21c3fead13fcedb8810b7895a11d1066581d
sws2apps <sws2apps@gmail.com>
2021-12-04 23:44:37 +0300

    Route sws-pocket/schedules

--- sws-pocket/sws-pocket.js
+++ sws-pocket/sws-pocket.js
@@ -1,5 +1,6 @@
 //app dependencies
 
+const fetch = require('node-fetch');
 require('../firebase-config'); //load firebase admin
 const Cryptr = require('cryptr');
 const { getFirestore } = require('firebase-admin/firestore');
@@ -68,6 +69,65 @@ router.get('/login', async (req, res)  => {
     }
 
     res.status(statusCode).send(JSON.stringify({message: statusMsg}))
+})
+
+// Get schedules
+router.get('/schedules', async (req, res)  => {
+    const congID = req.headers.cong_id;
+    const congPIN = req.headers.cong_pin;
+    const studentPIN = req.headers.user_pin;
+
+    let uri = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : 'https://sws2apps.herokuapp.com/';
+    uri += "api/sws-pocket/login";
+
+    fetch(uri, {
+        method: 'GET',
+        headers: {
+            'cong_id': congID,
+            'cong_pin': congPIN,
+            'user_pin': studentPIN, 
+        }
+    })
+    .then(async (response) => {
+        const data = await response.json();
+        if (response.status === 200) {
+            try {
+                let finalData = [];
+                const congRef = db.collection('congregation_public_data').doc(congID);
+                const docSnap = await congRef.get();
+
+                if (docSnap.exists && docSnap.data().schedLMMOA) {
+                    const data = docSnap.data().schedLMMOA;
+                    for(let i=0; i < data.length; i++) {
+                        let obj = {};
+            
+                        obj.id = data[i].id;
+                        obj.title = data[i].title;
+                        obj.date_received = data[i].dateSent;
+            
+                        const myKey = congID + "&lmm-oa_" + congPIN;
+                        const cryptr = new Cryptr(myKey);
+            
+                        try {
+                            const decryptedData = cryptr.decrypt(data[i].data);
+                            obj.schedule_data = JSON.parse(decryptedData);
+                        } catch {}
+                        
+                        finalData.push(obj);
+                    }
+                }
+
+                res.status(200).send(JSON.stringify({message: finalData}));
+            } catch {
+                res.status(500).send(JSON.stringify({message: 'Tsy mbola afaka maka fandaharana avy any amin’ny internet ianao izao.'}))
+            }
+        } else {
+            res.status(response.status).send(JSON.stringify({message: 'Tsy mbola afaka maka fandaharana avy any amin’ny internet ianao izao.'}))
+        }
+    })
+    .catch((error) => {
+        res.status(500).send(JSON.stringify({message: error.message}))
+    })    
 })
   
 module.exports = router;

875454960371dcc50b71d0c23a8b5ec9bb7c1f9e
Scheduling Workbox System <95527156+sws2apps@users.noreply.github.com>
2021-12-07 10:38:13 +0300

    lmm-oa: export router

--- lmm-oa/lmm-oa.js
+++ lmm-oa/lmm-oa.js
@@ -6,3 +6,5 @@ const router = express.Router();
 router.get('/createaccount', async (req, res) => {
     res.status(200).send(JSON.stringify({message: 'I have received you request, but will add more information to it later'}))
 })
+
+module.exports = router;
